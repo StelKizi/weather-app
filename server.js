0ffbb4946c2be6dfeafd39293427062b0050d522
StelKizi <stelkiziridou@gmail.com>
2020-04-27 17:41:52 +0200

    Set up the html file, functionality missing

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+const OPEN_WEATHER_KEY = process.env.OPEN_WEATHER_KEY;
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 5000;
 
+app.use(express.json());
 app.use(express.static('public'));
 
-//Put your endpoints here
+
+app.post('/weather', (req, res) => {
+
+})
 
 app.listen(port, () => console.log('Server listening on port ' + port));
