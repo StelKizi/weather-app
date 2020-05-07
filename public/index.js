a8f42656adfff23243d5944d14c2d9ec6d08ec27
StelKizi <stelkiziridou@gmail.com>
2020-05-08 00:00:53 +0200

    Getting 404 on /api

--- public/index.js
+++ public/index.js
@@ -1,40 +1,48 @@
 const searchTextArea = document.querySelector('#city-search-input-text');
 const searchButton = document.querySelector('#city-search-button');
+const output = document.querySelector('.container');
 
 const userLocation = () => {
   if('geolocation' in navigator) {
-    navigator.geolocation.getCurrentPosition(async position => {
+    navigator.geolocation.getCurrentPosition(position => {
+
       const latitude = position.coords.latitude;
       const longitude = position.coords.longitude;
+      const info = {latitude, longitude};
 
       const options = {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'Accept': 'application/json'
+          //'Accept': 'application/json'
         },
-        body: JSON.stringify({
-          latitude: latitude,
-          longitude: longitude
-        })
+        body: JSON.stringify(info),
       };
     
-      const res = await fetch('http://localhost:3000/weather', options);
-      const data = await res.json();
-      console.log(data);      
+      fetch('http://localhost:3000/api', options)
+      .then(res => res.json())
+      .then(res => appendWeatherData(res));
+      /* .then(res => appendWeatherData(res)); */
     });
   } else {
-    console.log('geolocation IS NOT available');
+    /* Geolocation is not available */
+    console.log('Geolocation is not available');
+    
   }
 }
 
-const retrieveData = () => {
-  const place = searchTextArea.value;
-  if (!place) return
+const appendWeatherData = (data) => {
+  output.innerHTML = `${data.name}`;
 }
 
-searchTextArea.addEventListener('keyup', e => e.code === 'Space' ? retrieveData() : '');
+const retrieveWeatherData = () => {
+  const cityName = searchTextArea.value;
+  if (!cityName) return
+  fetch(`http://localhost:3000/api/${cityName}`)
+    .then(res => res.json())
+    .then(res => appendWeatherData(res));
+}
 
-window.onload = () => {
-  userLocation();
-};
+searchTextArea.addEventListener('keyup', e => e.code === 'Space' ? retrieveWeatherData() : '');
+searchButton.addEventListener('click', retrieveWeatherData());
+window.onload = () => userLocation();
--- server.js
+++ server.js
@@ -6,18 +6,25 @@ const OPEN_WEATHER_API_KEY = process.env.OPEN_WEATHER_API_KEY;
 const express = require('express');
 const app = express();
 const port = 3000;
+//const axios = require('axios');
+const fetch = require ('node-fetch');
+const bodyParser = require('body-parser');
 
 app.use(express.json());
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/weather', (req, res) => {
-  const data = req.body;
-  res.json({
-    status: 'success',
-    latitude: data.latitude,
-    longitude: data.longitude
-  })
-  res.json();
+app.post('/weather', async (req, res) => {
+  const latitude = req.body.lat;
+  const longitude = req.body.lon;
+  const url = `api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${OPEN_WEATHER_API_KEY}`;
+  await axios({
+    url: url,
+    responseType: 'json'
+  }).then(data => res.json(data));
+  console.log(req.body)
+  .catch(err);  
 })
+
 
 app.listen(port, () => console.log('Server listening on port ' + port));
