cd62b167fc61e124160148ee6c2db0cfc48c8afa
StelKizi <stelkiziridou@gmail.com>
2020-05-14 19:27:12 +0200

    set up data, needs styling

--- public/index.js
+++ public/index.js
@@ -20,8 +20,48 @@ const userLocation = () => {
   }
 }
 
-const appendWeatherData = (data) => {
-  output.innerHTML = `${data.name}`;
+const appendWeatherData = (data) => { 
+
+  /* Extract hours and minutes from unix timestamp for sunrise and sunset */
+  let unix_timestamp_sunrise = data.sys.sunrise;
+  const date_sunrise = new Date(unix_timestamp_sunrise * 1000);
+  const hours_sunrise = date_sunrise.getHours() < 10 ? '0' + date_sunrise.getHours() : date_sunrise.getHours();
+  const minutes_sunrise = date_sunrise.getMinutes() < 10 ? '0' + date_sunrise.getMinutes() : date_sunrise.getMinutes();
+  const formattedTime_sunrise = `${hours_sunrise}:${minutes_sunrise}`;
+  
+  let unix_timestamp_sunset = data.sys.sunset;
+  const date_sunset = new Date(unix_timestamp_sunset * 1000);
+  const hours_sunset = date_sunset.getHours() < 10 ? '0' + date_sunset.getHours() : date_sunset.getHours();
+  const minutes_sunset = date_sunset.getMinutes() < 10 ? '0' + date_sunset.getMinutes() : date_sunset.getMinutes();
+  const formattedTime_sunset = `${hours_sunset}:${minutes_sunset}`;
+  
+  output.innerHTML = `
+    <div class="city-country">${data.name},${data.sys.country}</div>
+
+    <div class="icon-degrees-symbol">
+      <div class="icon"> 
+        <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">
+      </div>
+
+      <div class="degrees-value">${data.main.temp}&deg</div>
+    </div>
+
+    <div class="descroption">${data.weather[0].description}</div>
+
+    <div class="more-data">
+      <div class="more-data-cluster-one">
+        <div class="feels-like">feels like ${data.main.feels_like}&deg</div>
+        <div class="min-max">${data.main.temp_min}/${data.main.temp_max}</div>
+        <div class="humidiy">humidity ${data.main.humidity}%</div>
+      </div>
+
+      <div class="more-data-cluster-two">
+        <div class="wind">wind ${data.wind.speed} m/s</div>
+        <div class="sunrise">sunrise ${formattedTime_sunrise}</div>
+        <div class="sunrise">sunset ${formattedTime_sunset}</div>
+      </div>
+    </div>
+  `;
 }
 
 const retrieveWeatherData = () => {
@@ -29,9 +69,10 @@ const retrieveWeatherData = () => {
   if (!cityName) return
   fetch(`http://localhost:3000/api/${cityName}`)
     .then(res => res.json())
-    .then(res => appendWeatherData(res));
+    .then(res => appendWeatherData(res))
+    .catch(err => console.log('Error:', err));
 }
 
-searchTextArea.addEventListener('keyup', e => e.code === 'Space' ? retrieveWeatherData() : '');
-searchButton.addEventListener('click', retrieveWeatherData());
+searchTextArea.addEventListener('keyup', e => e.keyCode === 13 ? retrieveWeatherData() : 0);
+searchButton.addEventListener('click', retrieveWeatherData);
 window.onload = () => userLocation();
--- server.js
+++ server.js
@@ -7,24 +7,33 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const axios = require('axios');
-const bodyParser = require('body-parser');
 
 app.use(express.json());
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/api/:lat/:lon', async (req, res) => {
-  const latitude = req.body.lat;
-  const longitude = req.body.lon;
-  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${OPEN_WEATHER_API_KEY}`;
   try {
+    const latitude = req.params.lat;
+    const longitude = req.params.lon;
+    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${OPEN_WEATHER_API_KEY}`;
     const location = await axios.get(url);
-    const data = await location.json();
-    res.json(data);
+    res.json(location.data);    
+    console.log('Request:', location.data);
   } catch (error) {
     console.log('Error:', error.message);
   }
-  console.log(req.body);  
+})
+
+app.get('/api/:city', async(req, res) =>{
+  try {
+    const city = req.params.city;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${OPEN_WEATHER_API_KEY}`;
+    const location = await axios.get(url);
+    res.json(location.data);    
+    console.log('Request:', location.data);
+  } catch (error) {
+    console.log('Error:', error.message);
+  }
 })
 
 app.listen(port, () => console.log('Server listening on port ' + port));
